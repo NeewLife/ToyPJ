2d2f52acdb5889f3d71f2215575d6ebf0b0240ea
Neewlife <dhtpdud2009@gmail.com>
2023-03-10 06:21:29 +0900

    '스프링부트 실행'

--- src/main/java/com/project/Board/ToyPjApplication.java
+++ src/main/java/com/project/Board/ToyPjApplication.java
@@ -1,4 +1,4 @@
-package com.project.ToyPJ;
+package com.project.Board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/project/Board/ToyPjApplicationTests.java
+++ src/test/java/com/project/Board/ToyPjApplicationTests.java
@@ -1,4 +1,4 @@
-package com.project.ToyPJ;
+package com.project.Board;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
