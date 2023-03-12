ccd4508113bd47931ca76655ab93ca79b8907dda
Neewlife <dhtpdud2009@gmail.com>
2023-03-13 08:30:58 +0900

    화면 띄우기 성공

--- src/test/java/com/project/Board/PostMapperTest.java
+++ src/test/java/com/project/Board/PostMapperTest.java
@@ -21,6 +21,7 @@ public class PostMapperTest {
     @Test
     void save() {
         PostRequest params = new PostRequest();
+        params.setPostId(1);
         params.setTitle("1번 게시글 제목");
         params.setContent("1번 게시글 내용");
         params.setWriter("테스터");
@@ -42,6 +43,35 @@ public class PostMapperTest {
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Test
+    void update() {
+        // 1. 게시글 수정
+        PostRequest params = new PostRequest();
+        params.setPostId(1);
+        params.setTitle("1번 게시글 제목 수정합니다.");
+        params.setContent("1번 게시글 내용 수정합니다.");
+        params.setWriter("도뎡이");
+        params.setNoticeYn(true);
+        postMapper.update(params);
+
+        // 2. 게시글 상세정보 조회
+        PostResponse post = postMapper.findById(1);
+        try {
+            String postJson = new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(post);
+            System.out.println(postJson);
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void delete() {
+        System.out.println("삭제 이전의 전체 게시글 개수는 : " + postMapper.findAll().size() + "개입니다.");
+        postMapper.deleteById(1);
+        System.out.println("삭제 이후의 전체 게시글 개수는 : " + postMapper.findAll().size() + "개입니다.");
     }
 
 }
