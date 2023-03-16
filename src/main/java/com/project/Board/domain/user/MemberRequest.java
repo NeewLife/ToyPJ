cc3990ad3d1562919e7fb2fc43bdeb8c32f25318
NeewLife <dhtpdud2009@gmail.com>
2023-03-17 05:11:30 +0900

    시큐리티 DB 연동중

---
+++ src/main/java/com/project/Board/domain/user/MemberRequest.java
@@ -0,0 +1,10 @@
+package com.project.Board.domain.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberRequest {            // 얘는 Member를 생성, 수정때 쓸 객체
+
+}
--- src/main/java/com/project/Board/domain/user/UserMapper.java
+++ src/main/java/com/project/Board/domain/user/UserMapper.java
@@ -2,6 +2,11 @@ package com.project.Board.domain.user;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
-public class UserMapper {
+public interface UserMapper {
+    public List<Member> getUserById();
+    public static void userSignUp(Member member) {
+    }
 }
