f1f669194599ec90b878dec2ff97fa566bf6fce4
Neewlife <dhtpdud2009@gmail.com>
2023-03-20 19:07:08 +0900

    시큐리티 적용중(DB 연동한 회원가입 구현중..)

--- src/main/java/com/project/Board/domain/controllers/AuthorizationController.java
+++ src/main/java/com/project/Board/domain/controllers/AuthorizationController.java
@@ -3,12 +3,13 @@ package com.project.Board.domain.controllers;
 import com.project.Board.domain.user.Member;
 import com.project.Board.domain.user.MemberRequest;
 import com.project.Board.domain.user.MemberService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/auth")
 public class AuthorizationController {
     private final MemberService memberService;
@@ -23,9 +24,8 @@ public class AuthorizationController {
     }
 
     @PostMapping("/register")
-    public String userSignUp(Model model, final MemberRequest memberRequest){
+    public void userSignUp(Model model, final MemberRequest memberRequest){
         memberService.signup(memberRequest);
-
-        return "redirect:/post/login";
+        
     }
 }
